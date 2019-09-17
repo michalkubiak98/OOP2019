814c8e8ffb65c8f54213507c036afa5faebd01df
t00205890 <michalpkubiak@gmail.com>
2019-09-17 17:05:13 +0100

    test2

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,9 @@
 public class Test1 {
     public static void main(String[] args) {
+
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
         System.out.println("Hi");
     }
 }
